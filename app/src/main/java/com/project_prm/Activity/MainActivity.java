776a9694dcaade4ae7e3c1ac2c453d92bd6a042e
thongtq69 <thongtqse173366@fpt.edu.vn>
2024-10-21 12:58:45 +0700

    PRM_Group4

--- app/src/main/java/com/project_prm/Activity/MainActivity.java
+++ app/src/main/java/com/project_prm/Activity/MainActivity.java
@@ -1,26 +1,129 @@
-package com.project_prm.Activity;
+package com.example.foodappproject.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 
-import com.project_prm.R;
+import com.example.foodappproject.Adapter.CategoryAdapter;
+import com.example.foodappproject.Adapter.SliderAdapter;
+import com.example.foodappproject.Domain.Category;
+import com.example.foodappproject.Domain.SliderItems;
+import com.example.foodappproject.R;
+import com.example.foodappproject.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends BaseActivity {
+    ActivityMainBinding binding;
+    private String email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Intent intent = getIntent();
+        email = intent.getStringExtra("email");
+        binding.name.setText(email);
+        initCategory();
+        initBanner();
+        setVariable();
+    }
+
+    private void initBanner() {
+        DatabaseReference myRef = database.getReference("Banners");
+        binding.progressBarBanner.setVisibility(View.VISIBLE);
+        ArrayList<SliderItems> items = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue : snapshot.getChildren()){
+                        items.add(issue.getValue(SliderItems.class));
+                    }
+                    banners(items);
+                    binding.progressBarBanner.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
+    }
+
+    private void banners(ArrayList<SliderItems> items){
+        binding.viewpager2.setAdapter(new SliderAdapter(items, binding.viewpager2));
+        binding.viewpager2.setClipChildren(false);
+        binding.viewpager2.setClipToPadding(false);
+        binding.viewpager2.setOffscreenPageLimit(3);
+        binding.viewpager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+
+        binding.viewpager2.setPageTransformer(compositePageTransformer);
+    }
+    private void setVariable(){
+
+        binding.bottomMenu.setItemSelected(R.id.home, true);
+        binding.bottomMenu.setOnItemSelectedListener(i -> {
+            if (i == R.id.cart){
+                Intent intent = new Intent(new Intent(MainActivity.this, CartActivity.class));
+                intent.putExtra("uid", getIntent().getStringExtra("uid"));
+                startActivity(intent);
+            }
+            if(i == R.id.maps){
+                startActivity(new Intent(MainActivity.this, MapActivity.class));
+            }
+            if(i == R.id.profile){
+                Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
+                intent.putExtra("email", email);
+                startActivity(intent);
+            }
+        });
+        binding.checkOrderBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+            intent.putExtra("uid", getIntent().getStringExtra("uid"));
+            startActivity(intent);
+        });
+    }
+    private void initCategory() {
+        DatabaseReference myRef=database.getReference("Category");
+        binding.progressBarCategory.setVisibility(View.VISIBLE);
+        ArrayList<Category> list=new ArrayList<>();
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot issue:snapshot.getChildren()){
+                        list.add(issue.getValue(Category.class));
+                    }
+                    if(list.size()>0){
+                        binding.categoryView.setLayoutManager(new GridLayoutManager(MainActivity.this,3));
+                        binding.categoryView.setAdapter(new CategoryAdapter(list));
+                    }
+                    binding.progressBarCategory.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        
     }
 }
